d423c46cd6233369540e2e4b16c767430caff582
Abhishek Himanshu <abhishekhimanshu@amb2007888l.stryker.cn>
2026-01-29 15:32:35 +0530

    removing env files

--- config/mongoDBconnection.js
+++ config/mongoDBconnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectToMongoDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URL);
+        await mongoose.connect(process.env.DATABASE_URL||'');
         console.log("Connected to MongoDB successfully");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
